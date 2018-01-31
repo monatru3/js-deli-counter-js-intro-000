ee910b1e9a72a02cb6b27f6a7575cf3b2f345476
Simone D <monatru3@hotmail.com>
2018-01-31 20:27:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function currentLine(katzDeliLine){
     } else {
     for (var i=0; i<katzDeliLine.length; i++) {
     var name= katzDeliLine[i]
-    var spot=" "+katzDeliLine.indexOf(name)+1+". "
-    katzDeliLine.splice(i, 1, spot+name)
+    var spot=katzDeliLine.indexOf(name)+1+". "
+    katzDeliLine.splice(i, 1, " "+spot+name)
     }
     return `The line is currently: ${katzDeliLine.toString()}`
   }
